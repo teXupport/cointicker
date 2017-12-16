e1b086071e4ec86b88390620134f606936320dd2
DarethZ33 <dhumiston_13@hotmail.com>
2017-12-16 01:16:52 -0600

    CSS Pull & Unminify
    To begin integrating Bootstrap I pulled the CSS file from the server and unminified it. I repeated this process for the jquery-ui css file. This gives me more options for tweaking the page.

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+	<title>Custom Crypto Monitoring (cryptocompare.com API)</title>
+		<!--Meta Tags-->
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-		<title>Custom Crypto Monitoring (cryptocompare.com API)</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!--Styles-->
 		<link rel="stylesheet" href="http://www.atlasestateagents.co.uk/css/tether.min.css">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-		<link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+		<link rel="stylesheet" href="res/css/bootstrap.min.css">
+		<link rel="stylesheet" href="res/css/jquery-ui.css">
 		<link rel="stylesheet" type="text/css" href="./res/css/main.css">
+		<!--Scripts-->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 		<script src="./res/js/rawCoins.js"></script>
@@ -16,23 +20,33 @@
 		<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 	</head>
 	<body>
-		<div id = "controls">
-			<table class="table-responsive">
-				<tr>
-					<td><button id="font-size-inc">Bigger Text</button></td>
-					<td><button id="font-size-dec">Smaller Text</button></td>
-					<td></td>
-				</tr>
-				<tr>
-					<td colspan="3"><label for="chart-control">Enable charts (beta)?</label> <input id="chart-control" type="checkbox" /></td>
-				</tr>
-				<tr>
-					<td colspan="2"><input id="sym-text" /></td>
-					<td><button id="add-sym">Add Symbol</button></td>
-				</tr>
-			</table>
-		</div>
-		<span class="separator"> </span>
+	<div class="jumbotron text-center">
+	<table class="table-responsive">
+			</tr>
+			<tr>
+				<td>
+				<button id="font-size-inc"><img width="24" height="24" src="res/includes/images/icons/zoom-in-icon.png"></button>
+				<button id="font-size-dec"><img width="24" height="24" src="res/includes/images/icons/zoom-out-icon.png"></button>
+				</td>
+				<td></td>
+				<td colspan="2"><input id="sym-text" /></td>
+				<td><button id="add-sym">Add Symbol</button></td>
+				<td>
+			</tr>
+		</table>
+		<h1>Cointicker</h1>
+		<p>All the Cryptocurrencies in one convenient location.</p>
+
+		
+	</div>
+	<div id = "controls">
+		<table class="table-responsive">
+			<tr>
+				<td colspan="3"><label for="chart-control">Enable charts (beta)?</label> <input id="chart-control" type="checkbox" /></td>
+			</tr>
+		</table>
+	</div>
+	<span class="separator"> </span>
 		<div id="ticker">
 			<table id="coins-tb" class="table-responsive table-bordered">
 				<tr>
@@ -50,10 +64,5 @@
 			</table>
 		</div>
 	</body>
-	<style>
-		body {
-			background-color: rgb(180, 180, 180);
-		}
-	</style>
 	<script src="./res/js/ticker.js"></script>
 </html>
